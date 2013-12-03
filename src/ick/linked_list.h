06ec7757b430e424df71e2654f79988450bbed95
omochimetaru <omochi.metaru@gmail.com>
2013-12-03 23:10:23 +0900

    連結リスト途中

--- src/ick/linked_list.h
+++ src/ick/linked_list.h
@@ -9,6 +9,8 @@
 #pragma once
 
 #include <stdlib.h>
+#include "allcator.h"
+#include "memory.h"
 
 namespace ick{
 	template <typename T> class LinkedList;
@@ -22,31 +24,70 @@ namespace ick{
 		void set_next(const LinkedListNode<T> * next) { next_ = next; }
 		void set_prev(const LinkedListNode<T> * prev) { prev_ = prev; }
 	public:
-		LinkedListNode():value_(),next_(NULL),prev_(NULL){
+		LinkedListNode():
+		value_(),next_(NULL),prev_(NULL)
+		{
+		}
+		LinkedListNode(const T & value):
+		value_(value),next_(NULL),prev_(NULL)
+		{
 		}
 		const T & value() const { return value_; }
 		void set_value(const T & value) { value_ = value; }
-		LinkedListNode<T> * next() const { return next; }
-		LinkedListNode<T> * prev() const { return prev; }
+		LinkedListNode<T> * next() const { return next_; }
+		LinkedListNode<T> * prev() const { return prev_; }
 	};
 	
 	template <typename T> class LinkedList {
 	private:
 		typedef LinkedListNode<T> Node;
+		Allocator * allocator_;
 		int num_;
 		Node * first_;
 		Node * last_;
 		void set_first(Node * first) { first_ = first; }
 		void set_last(Node * last) { last_ = last; }
-		void InsertAfter(Node * node, Node * new_node) {
-			new_node->set_prev(node);
-			new_node->set_next(node->next());
-			if(!node->next()){
+
+
+	public:
+		LinkedList():
+		allocator_(g_static_allocator),num_(0),first_(NULL),last_(NULL)
+		{
+		}
+		LinkedList(Allocator * allocator):
+		allocator_(allocator),num_(0),first_(NULL),last_(NULL)
+		{
+		}
+		Allocator * allocator() const { return allocator_; }
+		int num() const { return num_; }
+		Node * first() const { return first_; }
+		Node * last() const { return last_; }
+		void InsertFirst(Node * new_node){
+			if(!first()){
+				set_first(new_node);
 				set_last(new_node);
+				new_node->set_prev(NULL);
+				new_node->set_next(NULL);
 			}else{
-				node->next()->set_prev(new_node);
+				InsertBefore(first(), new_node);
 			}
-			node->set_next(new_node);
+		}
+		Node * InsertFirst(const T & value){
+			Node * node = ICK_NEW_A(allocator(), Node, value);
+			InsertFirst(node);
+			return node;
+		}
+		void InsertLast(Node * new_node){
+			if(!last()){
+				InsertFirst(new_node);
+			}else{
+				InsertAfter(last(), new_node);
+			}
+		}
+		Node * InsertLast(const T & value){
+			Node * node = ICK_NEW_A(allocator(), Node, value);
+			InsertLast(node);
+			return node;
 		}
 		void InsertBefore(Node * node, Node * new_node) {
 			new_node->set_prev(node->prev());
@@ -58,29 +99,39 @@ namespace ick{
 			}
 			node->set_prev(new_node);
 		}
-		void InsertFirst(Node * new_node){
-			if(!first()){
-				set_first(new_node);
+		Node * InsertBefore(Node * node, const T & value){
+			Node * new_node = ICK_NEW_A(allocator(), Node, value);
+			InsertBefore(node, new_node);
+			return new_node;
+		}
+		void InsertAfter(Node * node, Node * new_node) {
+			new_node->set_prev(node);
+			new_node->set_next(node->next());
+			if(!node->next()){
 				set_last(new_node);
-				new_node->set_prev(NULL);
-				new_node->set_next(NULL);
 			}else{
-				InsertBefore(first(), new_node);
+				node->next()->set_prev(new_node);
 			}
+			node->set_next(new_node);
 		}
-		void InsertLast(Node * new_node){
-			if(!last()){
-				InsertFirst(new_node);
+		Node * InsertAfter(Node * node, const T & value){
+			Node * new_node = ICK_NEW_A(allocator(), Node, value);
+			InsertAfter(node, new_node);
+			return new_node;
+		}
+		//nodeはdeleteされる
+		void Remove(Node * node){
+			if(!node->prev()){
+				set_first(node->next());
 			}else{
-				InsertAfter(last(), new_node);
+				node->prev()->set_next(node->next());
 			}
+			if(!node->next()){
+				set_last(node->prev());
+			}else{
+				node->next()->set_prev(node->prev());
+			}
+			ICK_DELETE_A(allocator(), node);
 		}
-	public:
-		LinkedList():num_(0),first_(NULL),last_(NULL){
-		}
-		int num() const { return num_; }
-		Node * first() const { return first_; }
-		Node * last() const { return last_; }
-		
 	};
 }

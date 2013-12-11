9939d1fff845ee6f2cb3a1f3b0fc66a3dc9e3249
omochimetaru <omochi.metaru@gmail.com>
2013-12-12 02:04:48 +0900

    テストが起動できた。iOSの実装は要検討。

---
+++ xcode/MacTest/main.cpp
@@ -0,0 +1,10 @@
+//
+//  main.cpp
+//  IronCake
+//
+//  Created by おもちメタル on 2013/12/12.
+//  Copyright (c) 2013年 omochimetaru. All rights reserved.
+//
+
+#include "gtest_main.cc"
+
---
+++ xcode/iOSTest/main.mm
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  iOSTest
+//
+//  Created by おもちメタル on 2013/12/12.
+//  Copyright (c) 2013年 omochimetaru. All rights reserved.
+//
+
+//#include "gtest_main.cc"
+
+#include <stdio.h>
+
+#include "gtest/gtest.h"
+
+GTEST_API_ int main(int argc, char **argv) {
+	printf("Running main() from gtest_main.cc\n");
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}

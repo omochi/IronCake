0423c3b3277e71cba66b64c2071e452aa6dbe7c7
omochimetaru <omochi.metaru@gmail.com>
2014-01-24 01:26:36 +0900

    元のプロジェクトを削除

--- android/IronCake/IronCakeTest/src/main/build-jni.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ueo pipefail
-script_dir=$(cd $(dirname "$0"); pwd)
-cd "$script_dir"
-ndk-build \
-	--jobs=8 \
-	"$@"
--- android/IronCake/IronCakeTest/src/main/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-APP_BUILD_SCRIPT := $(NDK_PROJECT_PATH)/jni/Android.mk
-
-APP_PLATFORM := android-14
-APP_MODULES := IronCakeTest
-NDK_TOOLCHAIN_VERSION := clang
-APP_STL := stlport_static
-
-APP_CFLAGS += -Wno-implicit-exception-spec-mismatch

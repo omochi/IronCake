c7937528523b0d71128cb7a0679024ce6315c282
omochimetaru <omochi.metaru@gmail.com>
2014-01-28 00:57:50 +0900

    mathはじめ

--- src/ick/base/math.h
+++ src/ick/base/math.h
@@ -6,4 +6,21 @@
 //  Copyright (c) 2013年 omochimetaru. All rights reserved.
 //
 
+#pragma once
 
+namespace ick{
+	template <typename T> T Min(const T & a, const T & b);
+	template <typename T> T Max(const T & a, const T & b);
+	template <typename T> T Clamp(const T & x, const T & min, const T & max);
+	
+	
+	template <typename T> T Min(const T & a, const T & b){
+		return a < b ? a: b;
+	}
+	template <typename T> T Max(const T & a, const T & b){
+		return a > b ? a: b;
+	}
+	template <typename T> T Clamp(const T & x, const T & min, const T & max){
+		return x < min ? min: ( max < x ? max: x );
+	}
+}
